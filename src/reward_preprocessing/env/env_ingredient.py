87356cc7eed358e847fcb0256512c297c7789cd4
Erik Jenner <erik.jenner99@gmail.com>
2021-07-20 11:34:57 +0200

    Allow setting env options from CLI

--- src/reward_preprocessing/env/env_ingredient.py
+++ src/reward_preprocessing/env/env_ingredient.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 import gym
 from sacred import Ingredient
 from stable_baselines3.common.vec_env import DummyVecEnv
@@ -8,12 +10,13 @@ env_ingredient = Ingredient("env")
 @env_ingredient.config
 def config():
     name = "EmptyMaze-v0"
+    options = {"size": 6, "random_start": True}
     _ = locals()  # make flake8 happy
     del _
 
 
 @env_ingredient.capture
-def create_env(name: str, _seed: int):
-    env = DummyVecEnv([lambda: gym.make(name)])
+def create_env(name: str, _seed: int, options: Dict[str, Any]):
+    env = DummyVecEnv([lambda: gym.make(name, **options)])
     env.seed(_seed)
     return env
