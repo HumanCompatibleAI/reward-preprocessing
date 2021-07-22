e23a5e3c0ecdb4267a8be969e10b84942e4c1243
Erik Jenner <erik.jenner99@gmail.com>
2021-07-22 12:21:56 +0200

    Test both environments in pipeline tests

--- tests/test_training_pipeline.py
+++ tests/test_training_pipeline.py
@@ -5,16 +5,22 @@ from reward_preprocessing.create_rollouts import ex as create_rollouts_ex
 from reward_preprocessing.interpret import ex as interpret_ex
 from reward_preprocessing.train_agent import ex as train_agent_ex
 from reward_preprocessing.train_reward_model import ex as train_reward_model_ex
+from reward_preprocessing.utils import get_env_name
 
 
-def test_agent_training_experiment(tmp_path):
+def test_agent_training_experiment(env, tmp_path):
     # for now we just check that it works without errors
     train_agent_ex.run(
-        config_updates={"run_dir": str(tmp_path), "steps": 10, "num_frames": 10}
+        config_updates={
+            "run_dir": str(tmp_path),
+            "steps": 10,
+            "num_frames": 10,
+            "env.name": get_env_name(env),
+        }
     )
 
 
-def test_dataset_creation(agent_path):
+def test_dataset_creation(env, agent_path):
     with tempfile.TemporaryDirectory() as dirname:
         path = Path(dirname) / "dataset"
         create_rollouts_ex.run(
@@ -23,11 +29,12 @@ def test_dataset_creation(agent_path):
                 "save_path": str(path),
                 "train_samples": 10,
                 "test_samples": 10,
+                "env.name": get_env_name(env),
             }
         )
 
 
-def test_reward_training_experiment(data_path, tmp_path):
+def test_reward_training_experiment(env, data_path, tmp_path):
     train_reward_model_ex.run(
         config_updates={
             "run_dir": str(tmp_path),
@@ -36,11 +43,12 @@ def test_reward_training_experiment(data_path, tmp_path):
             "data_path": str(data_path),
             "steps": None,
             "test_steps": None,
+            "env.name": get_env_name(env),
         }
     )
 
 
-def test_interpret_experiment(model_path, agent_path, tmp_path):
+def test_interpret_experiment(env, model_path, agent_path, tmp_path):
     interpret_ex.run(
         config_updates={
             "run_dir": str(tmp_path),
@@ -48,5 +56,9 @@ def test_interpret_experiment(model_path, agent_path, tmp_path):
             "agent_path": str(agent_path),
             "sparsify.steps": 10,
             "sparsify.batch_size": 2,
+            "transition_visualization.num_samples": 10,
+            "transition_visualization.num": 2,
+            "rollout_visualization.plot_shape": (2, 2),
+            "env.name": get_env_name(env),
         }
     )
