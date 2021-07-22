368c1724054c5795a80a7e9b8d531b096d9ea710
Erik Jenner <erik.jenner99@gmail.com>
2021-07-22 09:38:22 +0200

    Refactor noise potentials
    From now on, there is no extra RandomPotentialShaping class; instead,
    any potential can be turned into a noise potential with the
    random_init() method (as long as that's possible for that type of potential).
    
    Also added a helper functions for instantiating potentials based on
    environment name.

--- src/reward_preprocessing/preprocessing/potential_shaping.py
+++ src/reward_preprocessing/preprocessing/potential_shaping.py
@@ -18,6 +18,7 @@ from torch import nn
 from reward_preprocessing.env.maze import get_agent_positions
 from reward_preprocessing.models import RewardModel
 from reward_preprocessing.transition import Transition
+from reward_preprocessing.utils import instantiate
 
 from .preprocessor import Preprocessor
 
@@ -53,6 +54,22 @@ class PotentialShaping(Preprocessor):
         )
         return rewards + self.gamma * next_potential - current_potential
 
+    def random_init(self, **kwargs) -> None:
+        """Set the potentials parameters randomly and freeze the weights.
+
+        The idea is to use this in order to get a noise potential,
+        sampled from the space of potentials representable by this class.
+        We freeze the weights because we usually don't want to train this
+        noise.
+        """
+        # Since the potential may in general be given by any Callable,
+        # we can't provide a general purpose implementation.
+        # Classes that support random initialization should override
+        # this method.
+        raise NotImplementedError(
+            "Random initialization is not implemented for this type of potential."
+        )
+
     def plot(self, env: gym.Env) -> plt.Figure:
         """Plot the potential if possible.
         The type of plot depends on the type of potential.
@@ -86,7 +103,6 @@ class PotentialShaping(Preprocessor):
         fig, ax = plt.subplots()
         im = ax.imshow(out)
         ax.set_axis_off()
-        ax.set(title="Learned potential")
         fig.colorbar(im, ax=ax)
         return fig
 
@@ -98,6 +114,11 @@ class LinearPotentialShaping(PotentialShaping):
         in_size = np.product(model.state_shape)
         potential = nn.Sequential(nn.Flatten(), nn.Linear(in_size, 1))
         super().__init__(model, potential, gamma)
+
+    def random_init(self, mean: float = 0, std: float = 1) -> None:
+        for param in self.model.parameters():
+            nn.init.normal_(param, mean=mean, std=std)
+            param.requires_grad = False
 
 
 class MlpPotentialShaping(PotentialShaping):
@@ -123,6 +144,11 @@ class MlpPotentialShaping(PotentialShaping):
         )
         super().__init__(model, potential, gamma)
 
+    def random_init(self, mean: float = 0, std: float = 1) -> None:
+        for param in self.model.parameters():
+            nn.init.normal_(param, mean=mean, std=std)
+            param.requires_grad = False
+
 
 class TabularPotentialShaping(PotentialShaping):
     """A preprocessor that adds a learned potential shaping in a tabular setting."""
@@ -140,6 +166,10 @@ class TabularPotentialShaping(PotentialShaping):
     def potential_data(self) -> torch.Tensor:
         return self._data
 
+    def random_init(self, mean: float = 0, std: float = 1) -> None:
+        nn.init.normal_(self._data, mean=mean, std=std)
+        self._data.requires_grad = False
+
     def plot(self, env: gym.Env) -> plt.Figure:
         fig, ax = plt.subplots()
 
@@ -150,18 +180,48 @@ class TabularPotentialShaping(PotentialShaping):
             .reshape(*env.observation_space.shape)
         )
         ax.set_axis_off()
-        ax.set(title="Learned potential")
         fig.colorbar(im, ax=ax)
         return fig
 
 
-class RandomPotentialShaping(TabularPotentialShaping):
-    """A preprocessor that adds a random potential shaping."""
+# dict mapping environment name to potential that will be used by default
+DEFAULT_POTENTIALS = {
+    "EmptyMaze-v0": "TabularPotentialShaping",
+    "MountainCar-v0": "LinearPotentialShaping",
+}
 
-    def __init__(
-        self, model: RewardModel, gamma: float, mean: float = 0, std: float = 1
-    ):
-        super().__init__(model, gamma)
-        self._data = torch.nn.Parameter(
-            std * torch.randn_like(self._data) + mean, requires_grad=False
-        )
+
+def instantiate_potential(
+    env_name: str = None, potential_name: str = None, **kwargs
+) -> PotentialShaping:
+    """Create the right PotentialShaping instance for a given environment.
+
+    Args:
+        env_name (str): the environment name, only used if no potential_name is
+            given
+        potential_name (str, optional): can be the name of a PotentialShaping
+            subclass, then that class is used. If not set (default), then
+            the function tries to choose a reasonable type of potential shaping
+            based on the environment.
+        **kwargs: passed on to the PotentialShaping constructor
+
+    Returns:
+        a PotentialShaping instance of the given or automatically determined type
+    """
+
+    if potential_name is None:
+        if env_name is None:
+            raise ValueError(
+                "Need to specify either a potential name or an environment."
+            )
+        if env_name not in DEFAULT_POTENTIALS:
+            raise ValueError(
+                f"No default potential shaping class for environment '{env_name}' "
+                "is set. You need to specify the type of potential to use "
+                "by setting sparsify.potential"
+            )
+        potential_name = DEFAULT_POTENTIALS[env_name]
+
+    return instantiate(
+        "reward_preprocessing.preprocessing.potential_shaping", potential_name, **kwargs
+    )
