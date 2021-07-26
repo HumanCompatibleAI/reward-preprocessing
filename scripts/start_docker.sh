a5312cfc99be530c842c21268d4dc83daa63ee0a
Erik Jenner <erik.jenner99@gmail.com>
2021-07-26 11:22:33 +0000

    Allow passing options through docker script

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -4,6 +4,9 @@
 # Docker container. $REWARD_PREPROCESSING_DIR must
 # be the path to the repository, it will be mounted
 # into the Docker container.
+# You may pass additional arguments to this script
+# which will be passed as options to docker run
+# (e.g. --cpus=2 --gpus 'device=2').
 
 # quit on errors, forbid undefined variables
 set -euo pipefail
@@ -12,5 +15,6 @@ docker run \
     --rm -it \
     --env MUJOCO_KEY_URL="$MUJOCO_KEY_URL" \
     --mount type=bind,src="$REWARD_PREPROCESSING_DIR",target=/reward_preprocessing \
+    "$@" \
     ejenner/reward_preprocessing:dependencies \
     ci/docker_setup.sh
