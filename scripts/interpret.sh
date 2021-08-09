73c51adedcb086b147072491e050aecba7014eb1
Erik Jenner <erik.jenner99@gmail.com>
2021-08-09 16:11:28 +0200

    Use stored dataset for sparsification helper

--- scripts/interpret.sh
+++ scripts/interpret.sh
@@ -8,5 +8,6 @@ env_name="$1"
 exec pipenv run python src/reward_preprocessing/interpret.py with \
     model_path="results/models/$env_name.pt" \
     agent_path="results/agents/$env_name" \
+    sparsify.data_path="results/data/$env_name" \
     "env.$env_name" \
     "${@:2}"
