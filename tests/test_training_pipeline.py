6f1cf044818842e566c29c2686772dd9bb57fffa
Erik Jenner <erik.jenner99@gmail.com>
2021-09-14 08:30:56 +0200

    Delete reward training files
    I probably won't need them again so it doesn't make much sense to fix
    them now.

--- tests/test_training_pipeline.py
+++ tests/test_training_pipeline.py
@@ -1,20 +1,5 @@
 from reward_preprocessing.interpret import ex as interpret_ex
-from reward_preprocessing.train_reward_model import ex as train_reward_model_ex
 from reward_preprocessing.utils import get_env_name
-
-
-def test_reward_training_experiment(env, data_path, tmp_path):
-    train_reward_model_ex.run(
-        config_updates={
-            "run_dir": str(tmp_path),
-            "epochs": 1,
-            "batch_size": 2,
-            "data_path": str(data_path),
-            "steps": None,
-            "test_steps": None,
-            "env.name": get_env_name(env),
-        }
-    )
 
 
 def test_interpret_experiment(env, model_path, tmp_path):
@@ -28,6 +13,7 @@ def test_interpret_experiment(env, model_path, tmp_path):
             "noise.enabled": True,
             "rewards.rollout_steps": 2,
             "rewards.bins": 2,
+            "rewards.steps": 4,
             "transition_visualization.steps": 10,
             "transition_visualization.num": 2,
             "rollout_visualization.plot_shape": (2, 2),
