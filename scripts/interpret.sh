18c47aac233399a61b1d2b5c083453c467e6ebe4
Erik Jenner <erik.jenner99@gmail.com>
2021-09-16 08:20:47 +0200

    Use cached rollouts

--- scripts/interpret.sh
+++ scripts/interpret.sh
@@ -7,7 +7,8 @@ env_name="$1"
 AGENT="results/agents/$env_name/model.zip"
 VALUE_NET="results/value_nets/$env_name/model.zip"
 MODEL="results/models/$env_name.pt"
-ROLLOUTS="[(0, \"$AGENT\", \"expert\"), (1, None, \"random\"), (0.5, \"$AGENT\", \"mixed\")]"
+ROLLOUT_PATH="results/rollouts/$env_name"
+ROLLOUTS="[(0, None, \"expert\", \"$ROLLOUT_PATH/expert.pkl\"), (0, None, \"random\", \"$ROLLOUT_PATH/random.pkl\"), (0, None, \"mixed\", \"$ROLLOUT_PATH/mixed.pkl\")]"
 
 exec xvfb-run poetry run python src/reward_preprocessing/interpret.py with \
     "model_path=$MODEL" \
