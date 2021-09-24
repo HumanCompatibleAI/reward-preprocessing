c5507f41876b6ffbfd4ccca04ae21acc2910c590
Erik Jenner <erik.jenner99@gmail.com>
2021-09-24 13:06:24 +0200

    Fix ground truth model

--- src/reward_preprocessing/models.py
+++ src/reward_preprocessing/models.py
@@ -29,47 +29,21 @@ class EmptyMazeRewardNet(RewardNet):
         return torch.as_tensor(rewards, device=state.device)
 
     def preprocess(
-        self, transitions: Transitions
+        self,
+        state: np.ndarray,
+        action: np.ndarray,
+        next_state: np.ndarray,
+        done: np.ndarray,
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         """Preprocess a batch of input transitions and convert it to PyTorch tensors.
 
         The output of this function is suitable for its forward pass,
         so a typical usage would be ``model(*model.preprocess(transitions))``.
         """
-        state_th = torch.as_tensor(
-            transitions.obs, device=self.device, dtype=torch.long
-        )
-        action_th = torch.as_tensor(transitions.acts, device=self.device)
-        next_state_th = torch.as_tensor(
-            transitions.next_obs, device=self.device, dtype=torch.long
-        )
-        done_th = torch.as_tensor(transitions.dones, device=self.device)
-
-        del transitions  # unused
+        state_th = torch.as_tensor(state, device=self.device, dtype=torch.long)
+        action_th = torch.as_tensor(action, device=self.device)
+        next_state_th = torch.as_tensor(next_state, device=self.device, dtype=torch.long)
+        done_th = torch.as_tensor(done, device=self.device)
 
         assert state_th.shape == next_state_th.shape
         return state_th, action_th, next_state_th, done_th
-
-    def predict(
-        self,
-        state: np.ndarray,
-        action: np.ndarray,
-        next_state: np.ndarray,
-        done: np.ndarray,
-    ) -> np.ndarray:
-        """Compute rewards for a batch of transitions without gradients.
-        Also performs some preprocessing and numpy conversion.
-        """
-        state_th = torch.as_tensor(state, device=self.device, dtype=torch.long)
-        action_th = torch.as_tensor(action, device=self.device)
-        next_state_th = torch.as_tensor(
-            next_state, device=self.device, dtype=torch.long
-        )
-        done_th = torch.as_tensor(done, device=self.device)
-
-        with torch.no_grad():
-            rew_th = self(state_th, action_th, next_state_th, done_th)
-
-        rew = rew_th.detach().cpu().numpy().flatten()
-        assert rew.shape == state.shape[:1]
-        return rew
