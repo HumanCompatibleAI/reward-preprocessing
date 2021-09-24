c9eab804f0c6081c93f49f374d48d1c9c8756699
Erik Jenner <erik.jenner99@gmail.com>
2021-09-24 12:54:58 +0200

    Parallelize preprocessing

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -7,6 +7,7 @@ DRLHP=0
 AIRL=0
 TRUTH=0
 FAST=""
+JOBS=1
 
 while [[ $# -gt 0 ]]; do
   key="$1"
@@ -34,20 +35,24 @@ while [[ $# -gt 0 ]]; do
       TRUTH=1
       shift
       ;;
+    -j|--jobs)
+      JOBS=$2
+      shift
+      shift
+      ;;
   esac
 done
 
+cmds=$(mktemp)
+trap "rm -f $cmds" 0 2 3 15
 
 if [[ $DRLHP == 1 ]]; then
   mkdir -p processed/preference_comparisons
   for size in 4 10; do
     MODEL_PATHS=$(find results/preference_comparisons -type f -path "*/empty_maze_$size*/final_reward_net.pt" -printf "%P\n" | sed 's/\/final_reward_net\.pt$//')
     for path in $MODEL_PATHS; do
-      echo "Processing results/preference_comparisons/$path"
-      poetry run python -m reward_preprocessing.optimize_tabular with \
-        env.empty_maze_$size $FAST \
-        "model_path=results/preference_comparisons/$path/final_reward_net.pt" \
-        "save_path=processed/preference_comparisons/$path"
+      echo "Adding results/preference_comparisons/$path"
+      echo "poetry run python -m reward_preprocessing.optimize_tabular with env.empty_maze_$size $FAST model_path=results/preference_comparisons/$path/final_reward_net.pt save_path=processed/preference_comparisons/$path" >> $cmds
     done
   done
 fi
@@ -57,11 +62,8 @@ if [[ $AIRL == 1 ]]; then
   for size in 4 10; do
     MODEL_PATHS=$(find results/adversarial -type f -path "*/empty_maze_$size*/checkpoints/final/reward_test.pt" -printf "%P\n" | sed 's/\/checkpoints\/final\/reward_test\.pt$//')
     for path in $MODEL_PATHS; do
-      echo "Processing results/adversarial/$path"
-      poetry run python -m reward_preprocessing.optimize_tabular with \
-        env.empty_maze_$size $FAST \
-        "model_path=results/adversarial/$path/checkpoints/final/reward_test.pt" \
-        "save_path=processed/adversarial/$path"
+      echo "Adding results/adversarial/$path"
+      echo "poetry run python -m reward_preprocessing.optimize_tabular with env.empty_maze_$size $FAST model_path=results/adversarial/$path/checkpoints/final/reward_test.pt save_path=processed/adversarial/$path" >> $cmds
     done
   done
 fi
@@ -71,11 +73,12 @@ if [[ $TRUTH == 1 ]]; then
   for size in 4 10; do
     MODEL_PATHS=$(find results/ground_truth_models -type f -path "*/empty_maze_$size*.pt" -printf "%P\n" | sed 's/\.pt$//')
     for path in $MODEL_PATHS; do
-      echo "Processing results/ground_truth_models/$path"
-      poetry run python -m reward_preprocessing.optimize_tabular with \
-        env.empty_maze_$size $FAST \
-        "model_path=results/ground_truth_models/$path.pt" \
-        "save_path=processed/ground_truth/$path"
+      echo "Adding results/ground_truth_models/$path"
+      echo "poetry run python -m reward_preprocessing.optimize_tabular with env.empty_maze_$size $FAST model_path=results/ground_truth_models/$path.pt save_path=processed/ground_truth/$path" >> ${cmds}
     done
   done
 fi
+
+parallel -j $JOBS --progress < ${cmds}
+
+rm ${cmds}
