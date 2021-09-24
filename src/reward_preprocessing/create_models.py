f58f018f54082a26f7a4cf1a5387ce680e19af25
Erik Jenner <erik.jenner99@gmail.com>
2021-09-24 12:55:11 +0200

    Update create_models.py

--- src/reward_preprocessing/create_models.py
+++ src/reward_preprocessing/create_models.py
@@ -1,19 +1,13 @@
+import itertools
+
 import torch
 
 from reward_preprocessing.models import EmptyMazeRewardNet
 
 if __name__ == "__main__":
-    model = EmptyMazeRewardNet()
-    torch.save(model, "results/ground_truth_models/empty_maze.pt")
-
-    model = EmptyMazeRewardNet(shaping="dense")
-    torch.save(model, "results/ground_truth_models/empty_maze_dense.pt")
-
-    model = EmptyMazeRewardNet(shaping="antidense")
-    torch.save(model, "results/ground_truth_models/empty_maze_antidense.pt")
-
-    model = EmptyMazeRewardNet(reward="path")
-    torch.save(model, "results/ground_truth_models/empty_maze_path.pt")
-
-    model = EmptyMazeRewardNet(reward="path", shaping="dense")
-    torch.save(model, "results/ground_truth_models/empty_maze_path_dense.pt")
+    sizes = [4, 10]
+    rewards = ["goal", "path"]
+    shapings = ["unshaped", "dense", "antidense", "random"]
+    for size, reward, shaping in itertools.product(sizes, rewards, shapings):
+        model = EmptyMazeRewardNet(size=size, reward=reward, shaping=shaping)
+        torch.save(model, f"results/ground_truth_models/empty_maze_{size}_{reward}_{shaping}.pt")
