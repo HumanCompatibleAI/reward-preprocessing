14f6e7cf78baf14ad7b4a27a1ffefc844eb9f2fc
Erik Jenner <erik.jenner99@gmail.com>
2021-09-25 17:00:57 +0200

    Finish mountain car plotting script

--- scripts/plot_mountain_car.sh
+++ scripts/plot_mountain_car.sh
@@ -38,23 +38,46 @@ ROLLOUT="(0, None, \"expert\", \"$ROLLOUT_PATH\")"
 mkdir -p fig/mountain_car
 
 if [[ $DRLHP == 1 ]]; then
-  echo ""
+  MODEL_PATHS=$(find processed/preference_comparisons -type f -path "*/shaped_mountain_car*_medium*.pt" -printf "%P\n" | sed 's/\.[a-z0-9_]*\.pt$//' | sort | uniq)
+  path_list=$(echo $MODEL_PATHS | sed 's/\(\S*\)/"\1",/g')
+  path_list="[$path_list]"
+  echo $path_list
+  for mode in l1 log; do
+    poetry run python -m reward_preprocessing.plot_reward_curves with \
+      env.mountain_car \
+      base_path=processed/preference_comparisons \
+      "model_base_paths=$path_list" \
+      save_path=fig/mountain_car/preference_comparisons_medium_$mode.pdf \
+      "rollout_cfg=$ROLLOUT" \
+      $mode
+  done
 fi
 
 if [[ $AIRL == 1 ]]; then
-  echo ""
-fi
-
-if [[ $TRUTH == 1 ]]; then
-  MODEL_PATHS=$(find processed/ground_truth -type f -path "*/shaped_mountain_car_*.pt" -printf "%P\n" | sed 's/\.[a-z0-9]*\.pt$//' | sort | uniq)
+  MODEL_PATHS=$(find processed/adversarial -type f -path "*/shaped_mountain_car*.pt" -printf "%P\n" | sed 's/\.[a-z0-9_]*\.pt$//' | sort | uniq)
   path_list=$(echo $MODEL_PATHS | sed 's/\(\S*\)/"\1",/g')
   path_list="[$path_list]"
   echo $path_list
   poetry run python -m reward_preprocessing.plot_reward_curves with \
     env.mountain_car \
-    base_path=processed/ground_truth \
+    base_path=processed/adversarial \
     "model_base_paths=$path_list" \
-    save_path=fig/mountain_car/ground_truth.pdf \
-    'rollout_cfg=(1, )'
-    # "rollout_cfg=$ROLLOUT"
+    save_path=fig/mountain_car/adversarial.pdf \
+    "rollout_cfg=$ROLLOUT"
+fi
+
+if [[ $TRUTH == 1 ]]; then
+  MODEL_PATHS=$(find processed/ground_truth -type f -path "*/shaped_mountain_car*.pt" -printf "%P\n" | sed 's/\.[a-z0-9_]*\.pt$//' | sort | uniq)
+  path_list=$(echo $MODEL_PATHS | sed 's/\(\S*\)/"\1",/g')
+  path_list="[$path_list]"
+  echo $path_list
+  for mode in l1 log; do
+    poetry run python -m reward_preprocessing.plot_reward_curves with \
+      env.mountain_car \
+      base_path=processed/ground_truth \
+      "model_base_paths=$path_list" \
+      save_path=fig/mountain_car/ground_truth_$mode.pdf \
+      "rollout_cfg=$ROLLOUT" \
+      $mode
+  done
 fi
