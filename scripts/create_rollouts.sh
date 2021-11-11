9eaf9cef808c4faa99589e03077b89bc312bc231
Erik Jenner <erik@Eriks-MacBook-Air.local>
2021-11-11 11:17:59 +0100

    Update dates after copying

--- scripts/create_rollouts.sh
+++ scripts/create_rollouts.sh
@@ -5,7 +5,8 @@ set -euxo pipefail
 
 env_name="$1"
 AGENT="results/expert_demos/$env_name/policies/final/model.zip"
-ROLLOUTS="[(0, \"$AGENT\", \"expert\")]"
+# ROLLOUTS="[(0, \"$AGENT\", \"expert\")]"
+ROLLOUTS="[(1, None, \"random\")]"
 
 exec xvfb-run poetry run python src/reward_preprocessing/create_rollouts.py with \
     "rollouts=$ROLLOUTS" \
--- scripts/plot_mountain_car.sh
+++ scripts/plot_mountain_car.sh
@@ -34,21 +34,23 @@ done
 
 ROLLOUT_PATH="results/expert_demos/seals_mountain_car/rollouts/final.pkl"
 ROLLOUT="(0, None, \"expert\", \"$ROLLOUT_PATH\")"
+DRLHP_ROLLOUT_PATH="old_results/rollouts/mountain_car/expert.pkl"
+DRLHP_ROLLOUT="(0, None, \"expert\", \"$DRLHP_ROLLOUT_PATH\")"
 
 mkdir -p fig/mountain_car
 
 if [[ $DRLHP == 1 ]]; then
-  MODEL_PATHS=$(find processed/preference_comparisons -type f -path "*/shaped_mountain_car*_medium*.pt" -printf "%P\n" | sed 's/\.[a-z0-9_]*\.pt$//' | sort | uniq)
+  MODEL_PATHS=$(find processed/preference_comparisons -type f -path "*/shaped_mountain_car*.pt" -printf "%P\n" | sed 's/\.[a-z0-9_]*\.pt$//' | sort | uniq)
   path_list=$(echo $MODEL_PATHS | sed 's/\(\S*\)/"\1",/g')
   path_list="[$path_list]"
   echo $path_list
   for mode in l1 log; do
-    poetry run python -m reward_preprocessing.plot_reward_curves with \
+    poetry run python -m reward_preprocessing.plot_drlhp_curves with \
       env.mountain_car \
       base_path=processed/preference_comparisons \
       "model_base_paths=$path_list" \
-      save_path=fig/mountain_car/preference_comparisons_medium_$mode.pdf \
-      "rollout_cfg=$ROLLOUT" \
+      save_path=fig/mountain_car/preference_comparisons_$mode.pdf \
+      "rollout_cfg=$DRLHP_ROLLOUT" \
       $mode
   done
 fi
